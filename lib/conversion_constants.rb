b3353b40f80f949ebff49b2ccaaa54508718ba81
Alyssa Diaz <alydiaz77@gmail.com>
2012-07-17 18:44:41 -0400

    added tests for threes and fours

--- lib/conversion_constants.rb
+++ lib/conversion_constants.rb
@@ -11,9 +11,19 @@ module ConversionConstants
 	" _|" +
 	"|_ "
 
+  THREE = " _ " +
+	  " _|" +
+	  " _|"
+
+  FOUR = "   " +
+	 "|_|" +
+	 "  |"
+
   CONVERSION_MAP = {
     ZERO => 0,
     ONE => 1,
-    TWO => 2
+    TWO => 2,
+    THREE => 3,
+    FOUR => 4
   } 
 end
--- spec/ocr_spec.rb
+++ spec/ocr_spec.rb
@@ -15,5 +15,12 @@ describe Ocr do
     subject.process_file('spec/test_data/twos.txt').should == '222222222'
   end
 
+  it "reads a file with one entry of threes" do
+    subject.process_file('spec/test_data/threes.txt').should == '333333333'
+  end 
+
+  it "reads a file with one entry of fours" do
+    subject.process_file('spec/test_data/fours.txt').should == '444444444'
+  end 
 end
 
