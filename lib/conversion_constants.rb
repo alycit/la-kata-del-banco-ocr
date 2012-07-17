c9336bb259331e9398410b935cc5c4ce9ca7a303
Alyssa Diaz <alydiaz77@gmail.com>
2012-07-17 18:55:43 -0400

    adding tests and code for 5-9

--- lib/conversion_constants.rb
+++ lib/conversion_constants.rb
@@ -8,22 +8,47 @@ module ConversionConstants
         "  |"
 
   TWO = " _ " +
-	" _|" +
-	"|_ "
+	      " _|" +
+	      "|_ "
 
   THREE = " _ " +
-	  " _|" +
-	  " _|"
+	        " _|" +
+	        " _|"
 
   FOUR = "   " +
-	 "|_|" +
-	 "  |"
+	       "|_|" +
+	       "  |"
+	       
+	FIVE = " _ " +
+	       "|_ " +
+	       " _|"
+	       
+	SIX = " _ " +
+	      "|_ " +
+	      "|_|"
+	      
+	SEVEN = " _ " +
+	        "  |" +
+	        "  |"
+	        
+	EIGHT = " _ " +
+	        "|_|" +
+	        "|_|"
+	        
+	NINE = " _ " +
+	       "|_|" +
+	       " _|"
 
   CONVERSION_MAP = {
-    ZERO => 0,
-    ONE => 1,
-    TWO => 2,
+    ZERO  => 0,
+    ONE   => 1,
+    TWO   => 2,
     THREE => 3,
-    FOUR => 4
+    FOUR  => 4,
+    FIVE  => 5,
+    SIX   => 6,
+    SEVEN => 7,
+    EIGHT => 8,
+    NINE  => 9
   } 
 end
--- spec/ocr_spec.rb
+++ spec/ocr_spec.rb
@@ -22,5 +22,25 @@ describe Ocr do
   it "reads a file with one entry of fours" do
     subject.process_file('spec/test_data/fours.txt').should == '444444444'
   end 
+
+  it "reads a file with one entry of fives" do
+    subject.process_file('spec/test_data/fives.txt').should == '555555555'
+  end 
+
+  it "reads a file with one entry of sixes" do
+    subject.process_file('spec/test_data/sixes.txt').should == '666666666'
+  end 
+
+  it "reads a file with one entry of sevens" do
+    subject.process_file('spec/test_data/sevens.txt').should == '777777777'
+  end 
+
+  it "reads a file with one entry of eights" do
+    subject.process_file('spec/test_data/eights.txt').should == '888888888'
+  end 
+
+  it "reads a file with one entry of nines" do
+    subject.process_file('spec/test_data/nines.txt').should == '999999999'
+  end 
 end
 
