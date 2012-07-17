92010e053af7f05d6576d556c9016ea1d3d9a6f9
Alyssa Diaz <alydiaz77@gmail.com>
2012-07-17 18:28:12 -0400

    Added test for twos

--- lib/conversion_constants.rb
+++ lib/conversion_constants.rb
@@ -7,8 +7,13 @@ module ConversionConstants
         "  |" +
         "  |"
 
+  TWO = " _ " +
+	" _|" +
+	"|_ "
+
   CONVERSION_MAP = {
     ZERO => 0,
-    ONE => 1
+    ONE => 1,
+    TWO => 2
   } 
 end
--- spec/ocr_spec.rb
+++ spec/ocr_spec.rb
@@ -11,5 +11,9 @@ describe Ocr do
     subject.process_file('spec/test_data/ones.txt').should == '111111111'
   end
 
+  it "reads a file with one entry of twos" do
+    subject.process_file('spec/test_data/twos.txt').should == '222222222'
+  end
+
 end
 
