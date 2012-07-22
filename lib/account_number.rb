b0f81bc1c499c61cf5ece8b7333b57e7c8ce3ee9
Alyssa Diaz <alydiaz77@gmail.com>
2012-07-22 07:52:57 -0500

    implemented simple implementation of checksum with tests

--- lib/account_number.rb
+++ lib/account_number.rb
@@ -33,6 +33,14 @@ class AccountNumber
   end
   
   def checksum
-    return 0
+    (@account_number[0].to_i * 9 +
+    @account_number[1].to_i * 8 +
+    @account_number[2].to_i * 7 +
+    @account_number[3].to_i * 6 +
+    @account_number[4].to_i * 5 +
+    @account_number[5].to_i * 4 +
+    @account_number[6].to_i * 3 +
+    @account_number[7].to_i * 2 + 
+    @account_number[8].to_i) % 11
   end
 end
--- spec/account_number_spec.rb
+++ spec/account_number_spec.rb
@@ -2,9 +2,64 @@ require 'spec_helper'
 require 'account_number'
 
 describe AccountNumber do
-  
-  it "it indicates that a number is valid" do
+
+  it "it indicates that all zeroes is valid" do
     subject.account_number = '000000000'
     subject.valid?.should be_true
+  end
+  
+  it "it indicates that '123456789' is valid" do
+    subject.account_number = '123456789'
+    subject.valid?.should be_true
+  end
+  
+  it "it indicates that '345882865' is valid" do
+    subject.account_number = '345882865'
+    subject.valid?.should be_true
+  end
+  
+  it "it indicates that '345882814' is valid" do
+    subject.account_number = '345882814'
+    subject.valid?.should be_true
+  end
+  
+  it "it indicates that '715882864' is valid" do
+    subject.account_number = '715882864'
+    subject.valid?.should be_true
+  end
+  
+  it "it indicates that '745882865' is invalid" do
+    subject.account_number = '745882865'
+    subject.valid?.should be_false
+  end
+  
+  it "it indicates that '745882869' is invalid" do
+    subject.account_number = '745882869'
+    subject.valid?.should be_false
+  end
+  
+  it "it indicates that '745882868' is invalid" do
+    subject.account_number = '745882868'
+    subject.valid?.should be_false
+  end
+  
+  it "it indicates that '123456788' is invalid" do
+    subject.account_number = '123456788'
+    subject.valid?.should be_false
+  end
+  
+  it "it indicates that '133456788' is invalid" do
+    subject.account_number = '133456788'
+    subject.valid?.should be_false
+  end
+  
+  it "it indicates that '133456780' is invalid" do
+    subject.account_number = '133456780'
+    subject.valid?.should be_false
+  end
+  
+  it "it indicates that a number is invalid when there are non numerics" do
+    subject.account_number = '664e71495'
+    subject.valid?.should be_false
   end
 end
