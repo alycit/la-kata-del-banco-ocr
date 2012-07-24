0b5abe4934962115c749d0de88b5010936737b9f
Alyssa Diaz <alydiaz77@gmail.com>
2012-07-23 21:14:24 -0400

    Took care of all code smells found in reek metrics

--- lib/account_number.rb
+++ lib/account_number.rb
@@ -8,7 +8,7 @@ require 'conversion_constants'
 class AccountNumber
   
   include ConversionConstants
-    
+  
   attr_accessor :account_number
   
   def initialize
@@ -17,7 +17,7 @@ class AccountNumber
   
   def extract_account_number set_of_lines
      set_of_lines.pop
-     set_of_lines.collect!{ |line| break_line_into_pieces_of_digit line }.transpose.join.chars.each_slice(9) do |text_digit_pieces|
+     set_of_lines.collect!{ |line| AccountNumber::break_line_into_pieces_of_digit line }.transpose.join.chars.each_slice(9) do |text_digit_pieces|
         @account_number << CONVERSION_MAP[text_digit_pieces.join]
      end
      @account_number
@@ -29,12 +29,10 @@ class AccountNumber
   
   private
   
-  def break_line_into_pieces_of_digit(line)
-    broken_line = []
-    line.chars.each_slice(3) do |three_chars|
-      broken_line << three_chars
+  def self.break_line_into_pieces_of_digit(line)
+    broken_line = line.chars.each_slice(3).inject([]) do |builder, three_chars|
+      builder << three_chars
     end
-    broken_line
   end
   
   def checksum
